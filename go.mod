c91556de1976e4ae193255804799f1381c93a9bc
naoyamaguchi <naoyamaguchi0128@gmail.com>
2020-02-04 07:39:44 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/naoyamaguchi/poet1
+
+go 1.13
+
+require (
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/goccy/go-yaml v1.2.0
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/moznion/gowrtr v1.4.1
+	golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 // indirect
+	golang.org/x/tools v0.0.0-20200131211209-ecb101ed6550 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	gopkg.in/go-playground/validator.v9 v9.30.0
+)
---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+
+	"github.com/goccy/go-yaml"
+	"github.com/moznion/gowrtr/generator"
+)
+
+// Document Object.
+type Document struct {
+	Version    string `yaml:"openapi"`
+	Components *Components
+}
+
+// Components Object
+type Components struct {
+	Schemas map[string]*Schema
+}
+
+// Schema Object
+type Schema struct {
+	Title            string
+	MultipleOf       int `yaml:"multipleOf"`
+	Maximum          int
+	ExclusiveMaximum bool `yaml:"exclusiveMaximum"`
+	Minimum          int
+	ExclusiveMinimum bool `yaml:"exclusiveMinimum"`
+	MaxLength        int  `yaml:"maxLength"`
+	MinLength        int  `yaml:"minLength"`
+	Pattern          string
+	MaxItems         int `yaml:"maxItems"`
+	MinItems         int `yaml:"minItems"`
+	MaxProperties    int `yaml:"maxProperties"`
+	MinProperties    int `yaml:"minProperties"`
+	Required         []string
+	Enum             []string
+
+	Type                 string
+	AllOf                []*Schema `yaml:"allOf"`
+	OneOf                []*Schema `yaml:"oneOf"`
+	AnyOf                []*Schema `yaml:"anyOf"`
+	Not                  *Schema
+	Items                *Schema
+	Properties           map[string]*Schema
+	AdditionalProperties *Schema `yaml:"additionalProperties"`
+	Description          string
+	Format               string
+	Default              string
+
+	Ref string `yaml:"$ref"`
+
+	Extension map[string]interface{}
+}
+
+// https://play.golang.org/p/Ew0IZYWcCQb
+var link = regexp.MustCompile("(^[A-Za-z])|_([A-Za-z])")
+
+func toCamelCase(str string) string {
+	return link.ReplaceAllStringFunc(str, func(s string) string {
+		return strings.ToUpper(strings.Replace(s, "_", "", -1))
+	})
+}
+
+func load(filePath string) (*Document, error) {
+	spec, _ := ioutil.ReadFile(filePath)
+	buf := bytes.NewBuffer(spec)
+
+	decoder := yaml.NewDecoder(buf, yaml.RecursiveDir(true), yaml.ReferenceDirs("spec"))
+
+	var document Document
+	_ = decoder.Decode(&document)
+
+	return &document, nil
+}
+
+func genStruct(schemas map[string]*Schema, root *generator.Root) (*generator.Root, error) {
+	for k1, v1 := range schemas {
+		oaiStruct := generator.NewStruct(toCamelCase(k1))
+		if v1.Type == "object" {
+			for k2, v2 := range v1.Properties {
+				switch v2.Type {
+				case "string":
+					oaiStruct = oaiStruct.AddField(toCamelCase(k2), "string", fmt.Sprintf("json:\"%s\"", k2))
+				case "integer":
+					oaiStruct = oaiStruct.AddField(toCamelCase(k2), "int", fmt.Sprintf("json:\"%s\"", k2))
+				case "boolean":
+					oaiStruct = oaiStruct.AddField(toCamelCase(k2), "bool", fmt.Sprintf("json:\"%s\"", k2))
+				case "object":
+					oaiStruct = oaiStruct.AddField(toCamelCase(k2), toCamelCase(k2), fmt.Sprintf("json:\"%s\"", k2))
+					m := map[string]*Schema{k2: v2}
+					root, _ = genStruct(m, root)
+				case "array":
+					switch v2.Items.Type {
+					case "string":
+						oaiStruct = oaiStruct.AddField(toCamelCase(k2), "[]string", fmt.Sprintf("json:\"%s\"", k2))
+					case "integer":
+						oaiStruct = oaiStruct.AddField(toCamelCase(k2), "[]int", fmt.Sprintf("json:\"%s\"", k2))
+					case "object":
+						oaiStruct = oaiStruct.AddField(toCamelCase(k2), "[]"+toCamelCase(k2), fmt.Sprintf("json:\"%s\"", k2))
+						m := map[string]*Schema{k2: v2.Items}
+						root, _ = genStruct(m, root)
+					default:
+						//
+					}
+				default:
+					//
+				}
+			}
+		}
+		root = root.AddStatements(oaiStruct).Gofmt("-s").Goimports()
+	}
+
+	return root, nil
+}
+
+func main() {
+	doc, _ := load("./spec/api.yaml")
+
+	root := generator.NewRoot(
+		generator.NewComment(" THIS CODE WAS AUTO GENERATED; DO NOT EDIT."),
+		generator.NewPackage("main"),
+		generator.NewNewline(),
+	)
+
+	root, _ = genStruct(doc.Components.Schemas, root)
+
+	generated, err := root.Generate(0)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(generated)
+
+}
